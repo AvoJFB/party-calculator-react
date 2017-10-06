6b7d728b7f1d2ab5818b8c810e4cdbcab04a08ac
arrtem <artem.fedorov@logicify.com>
2017-10-06 19:52:39 +0300

    git ignore

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
-// eslint-disable-next-line react/jsx-filename-extension
+// eslint-disable-next-line react/jsx-filename-extension.
 ReactDOM.render(<App />, document.getElementById('root'));
 
 if (module.hot) {
