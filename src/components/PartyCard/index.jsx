4b9f668510f98fdcc7ea0f902b3ff111fe156ad8
arrtem <artem.fedorov@logicify.com>
2017-10-18 18:46:34 +0300

    Party component

---
+++ src/components/PartyCard/index.jsx
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import { withStyles } from 'material-ui/styles';
+import Card, { CardActions, CardContent, CardMedia } from 'material-ui/Card';
+import { Button, Typography } from 'material-ui';
+
+const cardWidth = 230;
+const styles = {
+  card: {
+    width: cardWidth,
+    height: 350,
+    margin: 10,
+    position: 'relative',
+  },
+  media: {
+    height: 150,
+  },
+  actions: {
+    display: 'flex',
+    'flex-direction': 'row',
+    height: 'auto',
+    position: 'absolute',
+    width: cardWidth,
+    'justify-content': 'center',
+    bottom: 5,
+  },
+  btn: {
+    minWidth: '55px',
+  },
+};
+
+class PartyCard extends Component {
+  constructor(props) {
+    super(props);
+    this.classes = props.classes;
+    this.state = {};
+  }
+
+  render() {
+    const { party, classes } = this.props;
+    return (
+      <div>
+        <Card className={classes.card} raised color="primary">
+          <CardMedia className={classes.media} image={party.avatarUrl} title={party.name} />
+          <CardContent>
+            <Typography type="headline" component="h2">
+              {party.name}
+            </Typography>
+            <Typography component="p">
+              {party.description}
+            </Typography>
+          </CardContent>
+          <CardActions className={classes.actions}>
+            <Button dense raised color="default" className={classes.btn} tabIndex={-2}>
+              Edit
+            </Button>
+            <Button dense raised color="default" className={classes.btn} tabIndex={-1}>
+              Join
+            </Button>
+            <Button dense raised color="default" className={classes.btn} tabIndex={0}>
+              Remove
+            </Button>
+          </CardActions>
+        </Card>
+      </div>
+    );
+  }
+}
+
+export default withStyles(styles)(PartyCard);
--- src/components/views/Dashboard/index.jsx
+++ src/components/views/Dashboard/index.jsx
@@ -1,9 +1,28 @@
 import React, { Component } from 'react';
+import PartyCard from '../../PartyCard';
+import styles from './index.css';
 
 class Dashboard extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      parties: [
+        {
+          id: 1,
+          name: 'Friday party',
+          description: 'This party for fridays only',
+          avatarUrl: 'http://static.sfdict.com/editorial-content/55tpbg0qcsp4/1yyCc4qjcMAQGKkSIe0Kqu/42c4df2d9014' +
+          '6a9563f66d9cb4140d73/weather_graphic.jpg?w=175&h=148',
+        },
+        {
+          id: 2,
+          name: 'Monday party',
+          description: 'This party for mondays only, or for tuesdays, or every week',
+          avatarUrl: 'http://static.sfdict.com/editorial-content/55tpbg0qcsp4/1yyCc4qjcMAQGKkSIe0Kqu/42c4df2d901' +
+          '46a9563f66d9cb4140d73/weather_graphic.jpg?w=175&h=148',
+        },
+      ],
+    };
   }
 
   componentWillMount() {
@@ -15,7 +34,13 @@ class Dashboard extends Component {
 
   render() {
     return (
-      <div>Dashboard</div>
+      <div className={styles['dashboard-container']}>
+        {
+          this.state.parties.map(party => (
+            <PartyCard party={party} key={party.id.toString()} />
+          ))
+        }
+      </div>
     );
   }
 }
